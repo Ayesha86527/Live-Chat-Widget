355cd9ca23f6c0ccd316f7c60437fc5d02db16eb
Ayesha Noman <ayeshanoman490@gmail.com>
2025-09-08 10:56:44 +0500

    Update database.py

--- database.py
+++ database.py
@@ -2,6 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from dotenv import load_dotenv
+import os
 
 url_database=os.getenv('URL_DATABASE')
 
@@ -10,5 +11,6 @@ engine=create_engine(url_database)
 SessionLocal=sessionmaker(autocommit=False,autoflush=False,bind=engine)
 
 Base=declarative_base()
+
 
 
